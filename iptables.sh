2de25548c597fac4360d725789ecd453593a32c6
Kevin Kirsch <ranlvor@starletp9.de>
2015-02-08 00:26:08 +0100

    Minor syntax-error-fixes in iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -31,32 +31,33 @@ iptables -A OUTPUT -s 127.0.0.1 -j ACCEPT
 
 # Established, Related
 iptables -A INPUT -p ALL -i $NIC_PUBLIC -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -p ALL -i $NIC_PUBLIC -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -p ALL -o $NIC_PUBLIC -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A INPUT -p ALL -i $NIC_VPN -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -p ALL -i $NIC_VPN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -p ALL -o $NIC_VPN -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #################################
 
 ## INPUT
 
 # TCP/UDP Port 10000
-iptables -A INPUT -p ALL --dport 10000 -i $NIC_PUBLIC -j ACCEPT
+iptables -A INPUT -p TCP --dport 10000 -i $NIC_PUBLIC -j ACCEPT
+iptables -A INPUT -p UDP --dport 10000 -i $NIC_PUBLIC -j ACCEPT
 
 # TCP Port 22
 iptables -A INPUT -p TCP --dport 22 -i $NIC_PUBLIC -j ACCEPT
 
 # Allow DNS from all devices
-iptables -A INPUT -p UDP 53 -i $NIC_PUBLIC -j ACCEPT
+iptables -A INPUT -p UDP --dport 53 -i $NIC_PUBLIC -j ACCEPT
 
 # Allow INPUT and OUTPUT Bridge Interface
 iptables -A INPUT -i $NIC_BRIDGE -j ACCEPT
-iptables -A OUTPUT -i $NIC_BRIDGE -j ACCEPT
+iptables -A OUTPUT -o $NIC_BRIDGE -j ACCEPT
 
 ## OUTPUT
-iptables -A OUTPUT -p UDP -i eth0 --dport 53 -j ACCEPT
-iptables -A OUTPUT -p TCP -i eth0 --dport 80 -j ACCEPT
-iptables -A OUTPUT -p TCP -i eth0 --dport 433 -j ACCEPT
+iptables -A OUTPUT -p UDP -o eth0 --dport 53 -j ACCEPT
+iptables -A OUTPUT -p TCP -o eth0 --dport 80 -j ACCEPT
+iptables -A OUTPUT -p TCP -o eth0 --dport 433 -j ACCEPT
 
 #################################
 
