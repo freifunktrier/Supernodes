dc5d50200967a248f9350c0f295154b241b083b4
Kevin Kirsch <ranlvor@starletp9.de>
2015-02-09 18:41:58 +0100

    setup-routes.sh: Set routing table only if it is not set
    Now it doesn't matter if this skript is called to often, it always sets the rules correctly and every rule it sets is set exactly once.
    
    The lines outside of the if have to be outside because when the VPN goes down the routes to the dev are cleared. But they do no harm, if they are executed to often because the kernel will notice the dublicate routes and reject them.

--- setup-routes.sh
+++ setup-routes.sh
@@ -1,7 +1,11 @@
-echo 1 VPN >> /etc/iproute2/rt_tables
+#!/bin/sh
 
-#Packets from mesh are routet via VPN, not via main uplink
-ip rule add iif br-fftr table VPN
+if ! grep -q VPN /etc/iproute2/rt_tables; then
+	echo 1 VPN >> /etc/iproute2/rt_tables
+
+	#Packets from mesh are routet via VPN, not via main uplink
+	ip rule add iif br-fftr table VPN
+fi
 
 #172.31.240.0/20 is just pushed to the VPN, rest is routed via 172.31.240.1
 ip route add default via 172.31.240.1 dev tun0 table VPN
