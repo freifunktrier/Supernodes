4b3e1d5cd9da3c901e97994505cd09f94bbf6d00
Ranlvor <ranlvor@starletp9.de>
2015-04-01 17:22:39 +0200

    [late commit] removed old openvpn-ipv4-uplink-routes

--- setup-routes.sh
+++ setup-routes.sh
@@ -13,8 +13,8 @@ ip rule add iif br-fftr table VPN
 ip -6 rule add iif br-fftr table VPN
 
 #172.31.240.0/20 is just pushed to the VPN, rest is routed via 172.31.240.1
-ip route add default via 172.31.240.1 dev tun0 table VPN
-ip route add 172.31.240.0/20 dev tun0 table VPN
+#ip route add default via 172.31.240.1 dev tun0 table VPN
+#ip route add 172.31.240.0/20 dev tun0 table VPN
 
 #enable forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
