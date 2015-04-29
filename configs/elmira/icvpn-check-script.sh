388996675936016f3a0f68b96c4fe6e62e535d73
dracoTrier <github@basisbit.de>
2015-04-29 16:10:51 +0200

    fix cron path

--- configs/elmira/icvpn-check-script.sh
+++ configs/elmira/icvpn-check-script.sh
@@ -2,6 +2,7 @@
 if ! ping -c 5 10.207.0.220; then
 if ! ping -c 5 10.207.0.6; then
 logger Oh crap! Need to restart tinc now.
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 service tinc restart
 fi
 fi
