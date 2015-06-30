68c914cfe53abf6991ea7fb6bf35f15b18615a5b
Ranlvor <ranlvor@starletp9.de>
2015-07-01 01:20:18 +0200

    Added config for fftr-mesh-vpnlp
    This is low mtu strange port fastd with MTU 1200 and port 1723 (MS PPTP-VPN-Port) for the realy bad cased

--- iptables.sh
+++ iptables.sh
@@ -67,11 +67,13 @@ done
 
 ## INPUT
 
-# TCP/UDP Port 10000/10001 (fastd)
+# TCP/UDP Port 10000/10001/1723 (fastd)
 iptables -A INPUT -p TCP --dport 10000 -i $NIC_PUBLIC -j ACCEPT
 iptables -A INPUT -p UDP --dport 10000 -i $NIC_PUBLIC -j ACCEPT
 iptables -A INPUT -p TCP --dport 10001 -i $NIC_PUBLIC -j ACCEPT
 iptables -A INPUT -p UDP --dport 10001 -i $NIC_PUBLIC -j ACCEPT
+iptables -A INPUT -p TCP --dport 1723 -i $NIC_PUBLIC -j ACCEPT
+iptables -A INPUT -p UDP --dport 1723 -i $NIC_PUBLIC -j ACCEPT
 
 # TCP/UDP Port 655 (tinc)
 iptables -A INPUT -p TCP --dport 655 -i $NIC_PUBLIC -j ACCEPT
